936d64c5369e839cc59dd6a46d3d00170d611d27
Jonathan Scott <jonathan.scott@gmail.com>
2020-04-07 17:07:52 -0700

    min and max heap insert partially working

--- structures/Heap.js
+++ structures/Heap.js
@@ -5,7 +5,41 @@ class MaxHeap {
 }
 
 class MinHeap {
-    constructor(data) {
-        this.data = data
+    constructor(capacity) {
+        this.capacity = capacity
+        this.size = 0
+        this.storage = new Array(capacity)
+    }
+    insert(value) {
+        this.storage[this.size] = value
+        this.heapify()
+        this.size++
+    }
+    heapify() {
+        if(this.size % 2 === 0) {
+            let idx = this.size / 2
+            if (this.storage[this.size] < this.storage[idx]) {
+                let temp = this.storage[idx]
+                this.storage[idx] = this.storage[this.size]
+                this.storage[this.size] = temp
+            } 
+        }
+        else if(this.size % 2 !== 0) {
+            let idx = (this.size - 1) / 2
+            if (this.storage[this.size] < this.storage[idx]) {
+                let temp = this.storage[idx]
+                this.storage[idx] = this.storage[this.size]
+                this.storage[this.size] = temp
+            } 
+        }
     }
 }
+
+let h = new MinHeap(10)
+h.insert(4)
+h.insert(1)
+h.insert(3)
+h.insert(7)
+h.insert(5)
+h.insert(9)
+console.log(h)

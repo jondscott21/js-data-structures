3b95ec590d8609fe26262c916d1c3c5eb205c360
Jonathan Scott <jonathan.scott@gmail.com>
2020-04-13 14:27:49 -0700

    max heap working

--- structures/Heap.js
+++ structures/Heap.js
@@ -2,6 +2,56 @@ class MaxHeap {
     constructor(data) {
         this.data = data
     }
+    insert(value) {
+        this.storage[this.size] = value
+        this.heapify(this.size)
+        this.size++
+        if(this.size === this.capacity) {
+            this.increaseCapacity()
+        }
+    }
+    heapify(cur) {
+        console.log(cur)
+        let parentIdx = Math.floor(cur / 2)
+        if(cur > 0 && cur % 2 === 0) {
+            parentIdx--
+        }
+        let childIdx = cur
+        while(this.storage[childIdx] > this.storage[parentIdx]) {
+            let tempVal = this.storage[parentIdx]
+            this.storage[parentIdx] = this.storage[childIdx]
+            this.storage[childIdx] = tempVal
+
+            childIdx = parentIdx
+            parentIdx = Math.floor(parentIdx / 2)
+            if(childIdx > 0 && childIdx % 2 === 0) {
+                parentIdx--
+            }
+        }
+    }
+    increaseCapacity() {
+        let tempStorage = [...this.storage]
+        this.capacity *= 2
+        this.storage = new Array(this.capacity)
+        tempStorage.forEach((el, i) => this.storage[i] = el)
+    }
+    delete(value) {
+        for(let i = 0; i < this.storage.length; i++) {
+            if(this.storage[i] === value) {
+                this.storage[i] = undefined
+                if(this.storage[i+1] !== undefined) {
+                    this.storage[i] = this.storage[this.size-1]
+                    this.storage[this.size-1] = undefined
+                    let left = (i*2) + 1
+                    let right = (i*2) + 2
+                    let lower = Math.min(left, right)
+                    console.log(left, right, lower)
+                    this.heapify(lower)
+
+                }
+            }
+        }
+    }
 }
 
 class MinHeap {
